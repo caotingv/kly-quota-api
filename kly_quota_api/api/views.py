0e7816d65f9d6b44696c43849cfb015fea6f2648
caotingv <caotingv@qq.com>
2023-09-11 17:15:07 +0800

    add import_data

--- kly_quota_api/api/views.py
+++ kly_quota_api/api/views.py
@@ -1,4 +1,6 @@
 from flask import Blueprint
+from kly_quota_api.db import api
+from kly_quota_api.db.models import *
 
 quota_blue = Blueprint('quota', __name__)
 PATH_PREFIX = '/v1.0'
@@ -12,9 +14,11 @@ def index():
 
 @quota_blue.route('/quota')
 def get_quota():
-    """
-    Get quota information.
-    """
+    with api.get_session() as session:
+        result = session.query(Memory).filter(Memory.id == 1).all()
+        for obj in result:
+            print(obj.capacity_gb)
+
     return {
         'message': 'Hello, world!'
     }
