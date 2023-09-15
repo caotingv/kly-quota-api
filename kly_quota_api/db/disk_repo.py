d6f0eea2700d97d134c794c1f4012c421f385a79
caotingv <caotingv@qq.com>
2023-09-15 18:11:48 +0800

    add disk controller

--- kly_quota_api/db/disk_repo.py
+++ kly_quota_api/db/disk_repo.py
@@ -5,15 +5,3 @@ from kly_quota_api.db import models
 class DiskRepository(base_repo.BaseRepository):
 
     model_class = models.Disk
-
-    def get_by_interface_type(self, session, interface_type, **filters):
-        result_dicts = []
-
-        query = session.query(self.model_class)
-        devices = query.filter().filter_by(interface_type=interface_type, **filters).all()
-        
-        for device in devices:
-            disk_dict = device.to_dict()  # 将每个Disk对象转换为字典
-            result_dicts.append(disk_dict)
-
-        return result_dicts
